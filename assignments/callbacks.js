b87431282f426738245bc8d7c2b4a98a7e08742c
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-10-05 09:07:38 -0700

    figured out and fixed how to the callbacks assignment, with testing throughout, but not the stretch yet, that took too long :(

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -43,34 +43,68 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
+const test1 = (getLength(items,item=>items.length));
+console.log(test1);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length-1]);
 }
 
+const test2 = (last(items,item=>items[items.length-1]));
+console.log(test2);
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x+y);
+  let result = x+y;
+  return cb(result);
 }
+function printSum (sum){
+  console.log(sum);
+}
+const test3 = (sumNums(235,15654,printSum));
+console.log(test3);
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x*y);
+  let result = x*y;
+  return cb(result);
 }
+
+function printMult (mult){
+  console.log(mult);
+}
+const test4 = (multiplyNums(10,15,printMult));
+console.log(test4);
+
+
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  for (let i=0; i < list.length; i++) {
-    if (list.indexOf(item)) {
-      return cb(true);
-    } else {
-      return cb(false);
+  // function checkedPet(item,list) {
+    let something = false;
+    for (let i=0; i < list.length; i++) {
+      if (list[i].includes(item)) {
+        something = true;
+      } 
     }
-  }
+    cb(something);
+  // }
+  // cb(checkedPet(item,list));
 }
 
+function trueFalse(tf) {
+  console.log(tf);
+}
+
+const pets = ['cat', 'hamster', 'dog', 'mouse', 'hedgehod'];
+const test5 = contains('mouse', pets, trueFalse);
+console.log(test5);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
